b737cce1f1c5d04d1320fd50a5566bb5d96d67f1
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-04-18 18:09:23 +0100

    Update sumAll.js
    Getting closer to solution I think, May be interesting to look back at this when completed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,9 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    for(let i = start; i < end; i++) {
+        return start + i;
+    }
 
 };
-
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
