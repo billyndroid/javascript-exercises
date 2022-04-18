cdff17d931eb385b819b8e59096b487e6fc28e7e
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-04-18 16:49:32 +0100

    update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,9 @@
-const removeFromArray = function(array, element) {
+const removeFromArray = function(array, element, elementToo) {
     array = array.filter(item => item !== element);
+    array = array.filter(item => item !== elementToo);
     return array;
 
 };
-removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 1);
 // Do not edit below this line
 module.exports = removeFromArray;
