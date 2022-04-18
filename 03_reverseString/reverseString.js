89d527912adeb4fadfe4e4e6e182be8d2e9e3caa
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-04-18 14:50:07 +0100

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
+
+
 const reverseString = function() {
-
+    let string = '';
+    const splitString = string.split();
+    const reversed = splitString.reverse();
+    let backwardsString = reversed.join('');
+    return backwardsString;
 };
-
+reverseString('hello');
 // Do not edit below this line
 module.exports = reverseString;
