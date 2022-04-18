4c66a53413d0adf8a0d725aa1bb690e99e8c0b52
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-04-18 14:10:38 +0100

    update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var repeatedString = "";
+    while (num > 0) {
+        repeatedString += string; 
+        num--;
+    }
+    if(num < 0){
+        return "ERROR";
+    }
+    else {
+    return repeatedString; 
+    }
 };
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
